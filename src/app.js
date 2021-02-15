7788f06d44da52b3b799ba315d82dbe673943448
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2021-02-15 20:48:35 +0100

    Add /hello endpoint in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const message = {message: "Hello World"};
 
-app.use("/hello");
+app.get("/hello", (req, res) => {
+	res.send(message);
+});
 
 module.exports = app;
