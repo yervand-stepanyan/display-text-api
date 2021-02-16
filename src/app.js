0f9cb6d11e18ebba5f80adc1c4ac544f2e4cf145
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2021-02-16 15:06:45 +0100

    Configure cors options in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
 const Api = require("./api");
+
+app.use(morgan("dev"));
+
+app.use(cors({
+	origin: "*",
+	methods: ["GET"],
+	allowedHeaders: ["Content-Type"],
+	credentials: true,
+	optionsSuccessStatus: 200,
+	maxAge: -1
+}));
 
 app.use("/", Api);
 
